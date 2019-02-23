e252d8dfcb52f3cbd308639cae3df70cc64c74d6
jiffberzos <tedsbennett@gmail.com>
2019-02-23 23:23:16 +0000

    Taking pieces and turns works. Need to fix pawn taking, castling, check

--- chess.js
+++ chess.js
@@ -2,6 +2,7 @@ let pieces = [];
 let dragging = false;
 let dragged;
 let occupied_list = [];
+var turn = 1;
 
 function preload(){
     blackPawnImg = loadImage("media/blackPawn.png");
@@ -28,8 +29,9 @@ function draw() {
     background(255);
     squares();
     if(dragging){
-        dragged.x = mouseX + offsetX;
-        dragged.y = mouseY + offsetY;
+        dragged.x = constrain(mouseX + offsetX, 0, 7*width/8);
+        dragged.y = constrain(mouseY + offsetY, 0, 7*height/8);
+
         for(let i = 0; i<pieces.length; i++){
             if(dragged_index == i){
             }
@@ -42,7 +44,7 @@ function draw() {
     else{
         for(let i = 0; i<pieces.length; i++){
             image(pieces[i].img, pieces[i].x*width/8,pieces[i].y*height/8, width/8, height/8);
-         }   
+         }
     }
 }
 
@@ -68,33 +70,33 @@ function occupied(){
 
 function piecesSetup(){
     for(let i = 0; i<8; i++){
-        pieces[i] = new Pawn(i,1,whitePawnImg);
+        pieces[i] = new Pawn(i,1,"white");
     }
     for(let i = 16; i<24; i++){
-        pieces[i] = new Pawn(i-16,6,blackPawnImg);
+        pieces[i] = new Pawn(i-16,6,"black");
     }
 
-    pieces[8] = new Rook(0,0,whiteRookImg);
-    pieces[9] = new Rook(7,0,whiteRookImg);
-    pieces[24] = new Rook(0,7, blackRookImg);
-    pieces[25] = new Rook(7,7,blackRookImg);
+    pieces[8] = new Rook(0,0,"white");
+    pieces[9] = new Rook(7,0,"white");
+    pieces[24] = new Rook(0,7, "black");
+    pieces[25] = new Rook(7,7,"black");
 
 
-    pieces[10] = new Knight(1,0,whiteKnightImg);
-    pieces[11] = new Knight(6,0,whiteKnightImg);
-    pieces[26] = new Knight(1,7,blackKnightImg);
-    pieces[27] = new Knight(6,7,blackKnightImg);
+    pieces[10] = new Knight(1,0,"white");
+    pieces[11] = new Knight(6,0,"white");
+    pieces[26] = new Knight(1,7,"black");
+    pieces[27] = new Knight(6,7,"black");
 
 
-    pieces[12] = new Bishop(2,0,whiteBishopImg);
-    pieces[13] = new Bishop(5,0,whiteBishopImg);
-    pieces[28] = new Bishop(2,7,blackBishopImg);
-    pieces[29] = new Bishop(5,7,blackBishopImg);
+    pieces[12] = new Bishop(2,0,"white");
+    pieces[13] = new Bishop(5,0,"white");
+    pieces[28] = new Bishop(2,7,"black");
+    pieces[29] = new Bishop(5,7,"black");
 
-    pieces[14] = new Queen(3,0,whiteQueenImg);
-    pieces[15] = new King(4,0,whiteKingImg);
-    pieces[30] = new Queen(4,7,blackQueenImg);
-    pieces[31] = new King(3,7,blackKingImg);
+    pieces[14] = new Queen(3,0,"white");
+    pieces[15] = new King(4,0,"white");
+    pieces[30] = new Queen(4,7,"black");
+    pieces[31] = new King(3,7,"black");
 
     }
 
@@ -104,106 +106,157 @@ function mousePressed() {
     for (let i = 0; i < pieces.length; i ++){
         if (mouseX > pieces[i].x*width/8 && mouseX < pieces[i].x*width/8 + width/8 &&
             mouseY > pieces[i].y*height/8 && mouseY < pieces[i].y*height/8 + height/8) {
-            dragging = true;
-            dragged_index = i;
-            dragged = new Piece(pieces[i].x, pieces[i].y, pieces[i].img);
-            // If so, keep track of relative location of click to corner of rectangle
-            offsetX = dragged.x*width/8-mouseX;
-            offsetY = dragged.y*height/8-mouseY;
+            if (turn % 2 == 0 && pieces[i].color == "black" || turn % 2 == 1 && pieces[i].color == "white"){
+              dragging = true;
+              dragged_index = i;
+              dragged = new Piece(pieces[i].x, pieces[i].y, pieces[i].img);
+              // If so, keep track of relative location of click to corner of rectangle
+              offsetX = dragged.x*width/8-mouseX;
+              offsetY = dragged.y*height/8-mouseY;
+              }
             }
+
     }
 }
 
 function mouseReleased() {
-    // Quit dragging   
+    // Quit dragging
     new_x = floor((dragged.x + width/16)*8/width);
     new_y = floor((dragged.y + height/16)*8/height);
     if(pieces[dragged_index].allowedMove(new_x, new_y)){
         pieces[dragged_index].x = new_x;
         pieces[dragged_index].y = new_y;
+        turn += 1;
     }
     dragging = false;
 
 }
 
 function checkCollision(new_x, new_y, old_x, old_y){
-    if(new_x<old_x){
-        let x =new_x-1;
-        if(new_y<old_y){
-            for(let y = new_y+1; y<old_y; y++){
-                for(let j = 0; j<occupied.length; j++){
-                    if (createVector(x, y) == occupied[j]){
-                        return false;
-                    }
-                x--;
-                }
-            }
-        }
-        if(new_y>old_y){
-            for(let y = old_y+1; y<new_y; y++){
-                for(let j = 0; j<occupied.length; j++){
-                    if (createVector(x, y) == occupied[j]){
-                        return false;
-                    }
-                }
-                x--;
-            }
-        }else{
-            let x =new_x;
-            for(let y = old_y+1; y<new_y; y++){
-                for(let j = 0; j<occupied.length; j++){
-                    if (createVector(x, y) == occupied[j]){
-                        return false;
-                    }
-                }
-            }
-
-        }
-    }
-    if(new_x>old_x){
-        let x =old_x+1;
-        if(new_y<old_y){
-            for(let y = new_y+1; y<old_y; y++){
-                for(let j = 0; j<occupied.length; j++){
-                    if (createVector(x, y) == occupied[j]){
-                        return false;
-                    }
-                }
-                x++
-
-            }
-        }
-        if(new_y>old_y){
-            for(let y = old_y+1; y<new_y; y++){
-                for(let j = 0; j<occupied.length; j++){
-                    if (createVector(x, y) == occupied[j]){
-                        return false;
-                    }
-                }
-                x++;
-            }
-        }else{
-            let x =old_x;
-            for(let y = old_y+1; y<new_y; y++){
-                for(let j = 0; j<occupied.length; j++){
-                    if (createVector(x, y) == occupied[j]){
-                        return false;
-                    }
-                }
-            }
-
-        }
-    
-    }else{
-        let x =old_x;
-        for(let y = old_y+1; y<new_y; y++){
-            for(let j = 0; j<occupied.length; j++){
-                if (createVector(x, y) == occupied[j]){
-                    return false;
-                }
-            }
-        }
-
-    }
+  console.log(new_x,new_y,old_x,old_y)
+  if(Math.abs(new_x-old_x)==1 || Math.abs(new_y-old_y)==1){
     return true;
+  }
+  if(new_x<old_x){
+      let x =new_x+1;
+      if(new_y<old_y){
+        console.log("up-left")
+          for(let y = new_y+1; y<old_y; y++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                    console.log(occupied_list[j], j)
+                    return false;
+                  }
+              x++;
+              }
+          }
+      }
+      else if(new_y>old_y){
+        console.log("down-left")
+          for(let y = new_y-1; y<old_y; y++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                    console.log(occupied_list[j].x, x, occupied_list[j].y, y)
+                    console.log(occupied_list)
+                      return false;
+                  }
+              }
+              x++;
+          }
+      }else{
+        console.log("left")
+          let y =new_y;
+          for(let x = new_x+1; x<old_x; x++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                      return false;
+                  }
+              }
+          }
+
+      }
+  }else if(new_x>old_x){
+      let x =old_x+1;
+      if(new_y<old_y){
+        console.log('right-up')
+          for(let y = old_y-1; y<new_y; y++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                      return false;
+                  }
+              x++;
+              }
+          }
+      }
+      else if(new_y>old_y){
+        console.log('right-down')
+          for(let y = old_y+1; y<new_y; y++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                      return false;
+                  }
+              }
+              x++;
+          }
+      }else{
+        console.log('right')
+          let y =new_y;
+          for(let x = old_x+1; x<new_x; x++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                      return false;
+                  }
+              }
+          }
+
+      }
+
+  }else if(new_x==old_x){
+      let x =old_x;
+      if(new_y<old_y){
+        console.log("up")
+          for(let y = new_y+1; y<old_y; y++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                      return false;
+                  }
+              }
+          }
+      }
+      if(new_y>old_y){
+        console.log("down")
+          for(let y = old_y+1; y<new_y; y++){
+            console.log(x,y)
+              for(let j = 0; j<occupied_list.length; j++){
+                  if (x == occupied_list[j].x && y == occupied_list[j].y){
+                      return false;
+                  }
+              }
+          }
+      }
+  }
+  return true;
+}
+
+function checkOccupied(x,y){
+  for(let i = 0; i < occupied_list.length; i++){
+    if(x == occupied_list[i].x && y == occupied_list[i].y){
+      if(pieces[i].color != pieces[dragged_index].color){
+        pieces[i].x = 100;
+        console.log(pieces)
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+  return true;
 }
--- pieces.js
+++ pieces.js
@@ -1,63 +1,90 @@
-function Pawn(x, y, img){
+function Pawn(x, y, color){
     this.x = x;
     this.y = y;
-    this.img = img;
-    if(img == blackPawnImg){
-        this.color = -1
+    this.color = color;
+    if(this.color == "white"){
+      this.img = whitePawnImg;
+    }else{
+      this.img = blackPawnImg;
     }
-    if(img == whitePawnImg){
-        this.color = 1
+    if(this.color == "white"){
+      this.direction = 1;
+    }else{
+      this.direction = -1;
     }
 
     this.allowedMove = function(new_x, new_y){
         if (checkCollision(new_x, new_y, this.x, this.y)){
-            if(new_y - this.y == this.color && new_x == this.x){
+            if(new_y - this.y == this.direction && new_x == this.x){
+              if(checkOccupied(new_x,new_y)){
                 return true;
+              }
             }
         }
         return false;
 
     }
 }
-function Rook(x, y, img){
+function Rook(x, y, color){
     this.x = x;
     this.y = y;
-    this.img = img;
+    this.color = color;
+    if(this.color == "white"){
+      this.img = whiteRookImg;
+    }else{
+      this.img = blackRookImg;
+    }
 
     this.allowedMove = function(new_x, new_y){
         if (checkCollision(new_x, new_y, this.x, this.y)){
             if(new_x - this.x == 0 || new_y - this.y == 0){
+              if(checkOccupied(new_x,new_y)){
                 return true;
+              }
             }
         }
         return false;
 
     }
 }
-function Knight(x, y, img){
+function Knight(x, y, color){
     this.x = x;
     this.y = y;
-    this.img = img;
+    this.color = color;
+    if(this.color == "white"){
+      this.img = whiteKnightImg;
+    }else{
+      this.img = blackKnightImg;
+    }
 
     this.allowedMove = function(new_x, new_y){
-        if((Math.abs(new_x - this.x) == 2 && Math.abs(new_y - this.y) == 1) || 
+        if((Math.abs(new_x - this.x) == 2 && Math.abs(new_y - this.y) == 1) ||
         (Math.abs(new_x - this.x) == 1 && Math.abs(new_y - this.y) == 2)){
+          if(checkOccupied(new_x,new_y)){
             return true;
+          }
         }
         return false;
 
     }
 }
 
-function Bishop(x, y, img){
+function Bishop(x, y, color){
     this.x = x;
     this.y = y;
-    this.img = img;
+    this.color = color;
+    if(this.color == "white"){
+      this.img = whiteBishopImg;
+    }else{
+      this.img = blackBishopImg;
+    }
 
     this.allowedMove = function(new_x, new_y){
         if (checkCollision(new_x, new_y, this.x, this.y)){
             if(Math.abs(new_y - this.y) == Math.abs(new_x - this.x)){
+              if(checkOccupied(new_x,new_y)){
                 return true;
+              }
             }
         }
         return false;
@@ -65,31 +92,45 @@ function Bishop(x, y, img){
     }
 }
 
-function Queen(x, y, img){
+function Queen(x, y, color){
     this.x = x;
     this.y = y;
-    this.img = img;
+    this.color = color;
+    if(this.color == "white"){
+      this.img = whiteQueenImg;
+    }else{
+      this.img = blackQueenImg;
+    }
 
     this.allowedMove = function(new_x, new_y){
         if (checkCollision(new_x, new_y, this.x, this.y)){
             if((Math.abs(new_y - this.y) == Math.abs(new_x - this.x)) ||
             (new_x - this.x == 0 || new_y - this.y == 0)){
+              if(checkOccupied(new_x,new_y)){
                 return true;
+              }
             }
-        }   
+        }
         return false;
 
     }
 }
 
-function King(x, y, img){
+function King(x, y, color){
     this.x = x;
     this.y = y;
-    this.img = img;
+    this.color = color;
+    if(this.color == "white"){
+      this.img = whiteKingImg;
+    }else{
+      this.img = blackKingImg;
+    }
 
     this.allowedMove = function(new_x, new_y){
         if(Math.abs(new_y - this.y) <= 1 && Math.abs(new_x - this.x) <= 1){
+          if(checkOccupied(new_x,new_y)){
             return true;
+          }
         }
         return false;
 
